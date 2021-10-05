0a211f99f507ae6d8c5d6a1c4dd44c8854acd949
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-05 12:04:27 +0530

    refactor: Move create user controller to auth.controller.ts

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,18 +1,4 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-
-import { User } from './models/entities/user.entity';
-import { UserService } from './user.service';
-import { CreateUserDto } from './models/dto/create-user.dto';
-import { UserRoleValidationPipe } from 'src/validations/user/user-role-validation.pipe';
+import { Controller } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {
-
-    constructor(private readonly userService:UserService) { }
-
-    @Post()
-    @UsePipes(new UserRoleValidationPipe())
-    createUser(@Body() createUserDto:CreateUserDto): Promise<User> {
-        return this.userService.createUser(createUserDto)
-    } 
-}
+export class UserController { }
