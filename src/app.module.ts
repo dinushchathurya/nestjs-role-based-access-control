72b96892fbc800e3f2affea1491c0d4a465bbff5
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-05 10:07:01 +0530

    feat: Create auth module and peering in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -19,7 +20,8 @@ import { UserModule } from './modules/user/user.module';
         entities: ['dist/**/*.entity.js'],
         synchronize: true,
     }),
-    UserModule
+    UserModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
